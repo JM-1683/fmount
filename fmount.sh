7830e6f4f08520ef4abbb3a23d0ebc2cedd2442e
JM-1683 <144040476+JM-1683@users.noreply.github.com>
2023-10-29 19:39:12 -0700

    Create fmount.sh

---
+++ fmount.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+usage() {
+  cat <<EOF
+  Usage: fmount [disk] [mount location]
+  Example: fmount /dev/sdb1 /mounts/data
+  EOF
+}
+
+if [[ $# == 0 ]]; then
+  usage
+fi
+
+TO_MOUNT="$1"
+MOUNT_LOCATION="$2"
+ID=$(blkid | grep $TO_MOUNT | awk -F '"' '{print $6}')
+FINAL_ADDITION="UUID="$ID"  "$MOUNT_LOCATION"  "$FILESYSTEM"  defaults 0 0"
+echo "$FINAL_ADDITION >> /etc/fstab
