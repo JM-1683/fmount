8b3f318000fc50091e90441cdb667d3b8131a273
JM-1683 <144040476+JM-1683@users.noreply.github.com>
2023-11-03 18:51:54 -0700

    Update fmount.sh
    Syntax errors; cleaned code for best practice usage; formatting; commenting

--- fmount.sh
+++ fmount.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# Define the usage function
 usage() {
-cat <<EOF
+    cat <<EOF
 
 Usage: fmount [disk] [mount location]
 Example: fmount /dev/sdb1 /mounts/data
@@ -9,22 +10,59 @@ Example: fmount /dev/sdb1 /mounts/data
 EOF
 }
 
+# Check if the number of arguments is equal to 2, otherwise print usage
 if [[ $# != 2 ]]; then
-	usage
-	exit 1
+    usage
+    exit 1
 fi
 
+# Assign arguments to variables
 TO_MOUNT="$1"
 MOUNT_LOCATION="$2"
-ID=$(blkid | grep $TO_MOUNT | awk -F'"' '{print $4}')
-FILESYSTEM=$(blkid | grep $TO_MOUNT | awk -F '"' '{print $6}')
-FINAL_ADDITION="UUID="$ID"	"$MOUNT_LOCATION"	"$FILESYSTEM"	defaults 0 0" 
-echo "$FINAL_ADDITION" >> /etc/fstab
 
-STATUS=$(findmnt --verify)
-if [[ "$STATUS" != "Success, no errors or warnings detected" ]]; then
-	echo -e "\nErrors detected in mount verification.\nUndoing addition to /etc/fstab of:\n"
-	echo -e "$FINAL_ADDITION\n"
-	sed -i '$d' /etc/fstab
+# Check if the mount location directory exists, offer to create it if not
+if [[ ! -d "$MOUNT_LOCATION" ]]; then
+    echo -n "The directory $MOUNT_LOCATION does not exist. Create directory? (yes/no): "
+    read USER_DECISION
+    if [[ ! "$USER_DECISION" =~ ^(no|No|n|o)$ ]]; then
+        mkdir -p "$MOUNT_LOCATION"
+        if [[ $? -ne 0 ]]; then
+            echo "Directory creation failed. Exiting."
+            exit 1
+        fi
+    else
+        echo "OK. As directory does not exist, fmount cannot continue. Terminating."
+        exit 1
+    fi
 fi
 
+# Get UUID and FILESYSTEM for the disk
+ID=$(blkid -s UUID -o value "$TO_MOUNT")
+FILESYSTEM=$(blkid -s TYPE -o value "$TO_MOUNT")
+
+# Check if we successfully retrieved UUID and FILESYSTEM
+if [[ -z "$ID" || -z "$FILESYSTEM" ]]; then
+    echo "Unable to find UUID or FILESYSTEM for $TO_MOUNT. Exiting."
+    exit 1
+fi
+
+# Append the new fstab entry
+FINAL_ADDITION="UUID=$ID	$MOUNT_LOCATION	$FILESYSTEM	defaults	0	0"
+echo "Adding $FINAL_ADDITION to /etc/fstab. " | sudo tee -a /etc/fstab > /dev/null
+
+# Verify the fstab entries
+if ! sudo findmnt --verify > /dev/null 2>&1; then
+    echo "Errors detected in mount verification. Undoing addition to /etc/fstab."
+    echo "Faulty entry: $FINAL_ADDITION"
+    # Using 'tac' to reverse the lines (making the last line first) and 'sed' to delete the first occurrence
+    sudo tac /etc/fstab | sed '0,/^UUID=$ID\s/ s///' | tac | sudo tee /etc/fstab > /dev/null
+    exit 1
+fi
+
+# Mount the filesystem
+if ! sudo mount "$MOUNT_LOCATION"; then
+    echo "Failed to mount $MOUNT_LOCATION. Check /etc/fstab for incorrect entries."
+    exit 1
+fi
+
+echo "Mount successful."
