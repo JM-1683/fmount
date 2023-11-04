eeccb7ddea758dacceaef2d4472a259ea55d6dd8
JM-1683 <144040476+JM-1683@users.noreply.github.com>
2023-11-03 18:18:54 -0700

    Update fmount.sh
    - As only two arguments are needed (but exactly two), the usage is displayed if there are more or less than two arguments.
    - Added error checking if what was added to /etc/fstab is invalid and will lead to an error-throwing mount

--- fmount.sh
+++ fmount.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 usage() {
-  cat <<EOF
-  Usage: fmount [disk] [mount location]
-  Example: fmount /dev/sdb1 /mounts/data
-  EOF
+cat <<EOF
+
+Usage: fmount [disk] [mount location]
+Example: fmount /dev/sdb1 /mounts/data
+
+EOF
 }
 
-if [[ $# == 0 ]]; then
-  usage
+if [[ $# != 2 ]]; then
+	usage
+	exit 1
 fi
 
 TO_MOUNT="$1"
 MOUNT_LOCATION="$2"
-ID=$(blkid | grep $TO_MOUNT | awk -F '"' '{print $6}')
-FINAL_ADDITION="UUID="$ID"  "$MOUNT_LOCATION"  "$FILESYSTEM"  defaults 0 0"
+ID=$(blkid | grep $TO_MOUNT | awk -F'"' '{print $4}')
+FILESYSTEM=$(blkid | grep $TO_MOUNT | awk -F '"' '{print $6}')
+FINAL_ADDITION="UUID="$ID"	"$MOUNT_LOCATION"	"$FILESYSTEM"	defaults 0 0" 
 echo "$FINAL_ADDITION" >> /etc/fstab
+STATUS=$(findmnt --verify)
+
+if [[ "$STATUS" != "Success, no errors or warnings detected" ]]; then
+echo -e "\nErrors detected in mount verification.\nUndoing addition to /etc/fstab of:\n"
+echo -e "$FINAL_ADDITION\n"
+sed -i '$d' /etc/fstab
+fi
+
